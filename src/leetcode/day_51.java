633a676a02671160ddbc556aeb6a7221ea65dc6e
Prateek-v-coder <vprateek738@gmail.com>
2026-08-12 01:09:14 +0530

    Day 51 revise 2

--- src/leetcode/day_51.java
+++ src/leetcode/day_51.java
@@ -60,4 +60,47 @@ public class day_51 {
         return dp[text1.length()][text2.length()];
 
     }// beats 48 % bas thoda sa help lena pada
+    public int solve(int i, int j , String s, int[][]dp){
+        if(i>j) return 0;
+        if(i==j) return 1;
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(s.charAt(i)==s.charAt(j)){
+            return dp[i][j] = 2 + solve(i+1,j-1,s,dp);
+        }else{
+            return dp[i][j] = Math.max(solve(i+1,j,s,dp),solve(i,j-1,s,dp));
+        }
+    }
+
+    public int longestPalindromeSubseq(String s) {
+        int[][] dp =  new int[s.length()][s.length()];
+        for(int[] i:dp){
+            Arrays.fill(i,-1);
+        }
+        return solve(0,s.length()-1,s,dp);
+
+    }// beats 86% bus thoda sa help lena pada
+    class Solution {
+        public int solve(int i , int j , String s, String str, int[][]dp){
+            if(i<0) return j+1;
+            if(j<0) return i+1;
+            if(dp[i][j]!=-1) return dp[i][j];
+            if(s.charAt(i)==str.charAt(j)){
+                return dp[i][j] = solve(i-1,j-1,s,str,dp);
+            }else{
+                int ins = 1+ solve(i,j-1,s,str,dp);
+                int del = 1+ solve(i-1,j,s,str,dp);
+                int rep = 1+ solve(i-1,j-1,s,str,dp);
+                return dp[i][j] = Math.min(ins,Math.min(del,rep));
+            }
+        }
+
+        public int minDistance(String word1, String word2){
+            int[][] dp = new int[word1.length()][word2.length()];
+            for(int[]i:dp){
+                Arrays.fill(i,-1);
+            }
+            return solve(word1.length()-1,word2.length()-1,word1,word2,dp);
+
+        }
+    }// beats 98% bas thoda help lena pada haiand leetcode 743 nahi kar paya hu aaj
     }
